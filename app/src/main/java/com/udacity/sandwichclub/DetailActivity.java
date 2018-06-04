5f71df403c536db502e20e99b5e51193c3ca0f9c
vishal <vishal3bhati@gmail.com>
2018-06-04 19:57:51 +0530

    Modify the project

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -1,12 +1,14 @@
 package com.udacity.sandwichclub;
 
 import android.content.Intent;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
+import com.udacity.sandwichclub.databinding.ActivityDetailBinding;
 import com.udacity.sandwichclub.model.Sandwich;
 import com.udacity.sandwichclub.utils.JsonUtils;
 
@@ -14,13 +16,16 @@ public class DetailActivity extends AppCompatActivity {
 
     public static final String EXTRA_POSITION = "extra_position";
     private static final int DEFAULT_POSITION = -1;
+    private ActivityDetailBinding mActivityDetailBinding;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_detail);
+        mActivityDetailBinding = DataBindingUtil.setContentView(this, R.layout.activity_detail);
 
         ImageView ingredientsIv = findViewById(R.id.image_iv);
+
 
         Intent intent = getIntent();
         if (intent == null) {
@@ -43,7 +48,7 @@ public class DetailActivity extends AppCompatActivity {
             return;
         }
 
-        populateUI();
+        populateUI(sandwich);
         Picasso.with(this)
                 .load(sandwich.getImage())
                 .into(ingredientsIv);
@@ -56,7 +61,20 @@ public class DetailActivity extends AppCompatActivity {
         Toast.makeText(this, R.string.detail_error_message, Toast.LENGTH_SHORT).show();
     }
 
-    private void populateUI() {
+    private void populateUI(Sandwich sandwich) {
+        mActivityDetailBinding.descriptionTv.setText(sandwich.getDescription());
+        mActivityDetailBinding.originTv.setText(sandwich.getPlaceOfOrigin());
+        for (String eachItem : sandwich.getIngredients()) {
+            mActivityDetailBinding.ingredientsTv.append(eachItem);
+            mActivityDetailBinding.ingredientsTv.append(",");
+
+        }
+        for (String eachItem : sandwich.getAlsoKnownAs()) {
+            mActivityDetailBinding.alsoKnownTv.append(eachItem);
+            mActivityDetailBinding.alsoKnownTv.append(",");
+        }
+
+        // {"name":{"mainName":"Ham and cheese sandwich","alsoKnownAs":[]},"placeOfOrigin":"","description":"A ham and cheese sandwich is a common type of sandwich. It is made by putting cheese and sliced ham between two slices of bread. The bread is sometimes buttered and/or toasted. Vegetables like lettuce, tomato, onion or pickle slices can also be included. Various kinds of mustard and mayonnaise are also common.","image":"https://upload.wikimedia.org/wikipedia/commons/thumb/5/50/Grilled_ham_and_cheese_014.JPG/800px-Grilled_ham_and_cheese_014.JPG","ingredients":["Sliced bread","Cheese","Ham"]}
 
     }
 }
--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -1,10 +1,51 @@
 package com.udacity.sandwichclub.utils;
 
+import android.util.Log;
+
 import com.udacity.sandwichclub.model.Sandwich;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class JsonUtils {
 
+    private static final String LOG_TAG = JsonUtils.class.getSimpleName();
+
     public static Sandwich parseSandwichJson(String json) {
+        try {
+            JSONObject mainObject = new JSONObject(json);
+            JSONObject nameObject = mainObject.getJSONObject("name");
+            String placeOfOrigin = mainObject.getString("placeOfOrigin");
+            String description = mainObject.getString("description");
+            String imageUrl = mainObject.getString("image");
+            JSONArray ingredients = mainObject.getJSONArray("ingredients");
+            String mainName = nameObject.getString("mainName");
+            JSONArray alsoKnownAsArray = nameObject.getJSONArray("alsoKnownAs");
+
+
+            List<String> ingredientsList = new ArrayList<String>();
+            if (ingredients != null) {
+                for (int i = 0; i < ingredients.length(); i++) {
+                    ingredientsList.add(ingredients.getString(i));
+                }
+            }
+
+            List<String> alsoKnownAsList = new ArrayList<String>();
+            if (alsoKnownAsArray != null) {
+                for (int i = 0; i < alsoKnownAsArray.length(); i++) {
+                    alsoKnownAsList.add(alsoKnownAsArray.getString(i));
+                }
+            }
+
+            return new Sandwich(mainName, alsoKnownAsList, placeOfOrigin, description, imageUrl, ingredientsList);
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
         return null;
     }
 }

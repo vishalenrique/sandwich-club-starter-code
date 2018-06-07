d0cd783ee2a50bdd04a77aff2865a142b4a32ca0
vishal <vishal3bhati@gmail.com>
2018-06-07 21:01:38 +0530

    Update UI

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.widget.ImageView;
 import android.widget.Toast;
 
@@ -11,6 +12,8 @@ import com.squareup.picasso.Picasso;
 import com.udacity.sandwichclub.databinding.ActivityDetailBinding;
 import com.udacity.sandwichclub.model.Sandwich;
 import com.udacity.sandwichclub.utils.JsonUtils;
+
+import java.util.List;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -62,16 +65,43 @@ public class DetailActivity extends AppCompatActivity {
     }
 
     private void populateUI(Sandwich sandwich) {
-        mActivityDetailBinding.descriptionTv.setText(sandwich.getDescription());
-        mActivityDetailBinding.originTv.setText(sandwich.getPlaceOfOrigin());
-        for (String eachItem : sandwich.getIngredients()) {
-            mActivityDetailBinding.ingredientsTv.append(eachItem);
-            mActivityDetailBinding.ingredientsTv.append(",");
 
+        String description = sandwich.getDescription();
+        if (!TextUtils.isEmpty(description)) {
+            mActivityDetailBinding.descriptionTv.setText(description);
+        } else {
+            mActivityDetailBinding.descriptionTv.setText(getString(R.string.placeHolder));
         }
-        for (String eachItem : sandwich.getAlsoKnownAs()) {
-            mActivityDetailBinding.alsoKnownTv.append(eachItem);
-            mActivityDetailBinding.alsoKnownTv.append(",");
+
+        String placeOfOrigin = sandwich.getPlaceOfOrigin();
+        if (!TextUtils.isEmpty(placeOfOrigin)) {
+            mActivityDetailBinding.originTv.setText(placeOfOrigin);
+        } else {
+            mActivityDetailBinding.originTv.setText(getString(R.string.placeHolder));
+        }
+
+        List<String> ingredients = sandwich.getIngredients();
+        if(ingredients.size()>0) {
+            for (int i = 0; i < ingredients.size(); i++) {
+                mActivityDetailBinding.ingredientsTv.append(ingredients.get(i));
+                if (i != (ingredients.size() - 1)) {
+                    mActivityDetailBinding.ingredientsTv.append("\n");
+                }
+            }
+        }else{
+            mActivityDetailBinding.ingredientsTv.append(getString(R.string.placeHolder));
+        }
+
+        List<String> alsoKnownAsList = sandwich.getAlsoKnownAs();
+        if(alsoKnownAsList.size()>0) {
+            for (int i = 0; i < alsoKnownAsList.size(); i++) {
+                mActivityDetailBinding.alsoKnownTv.append(alsoKnownAsList.get(i));
+                if (i != (alsoKnownAsList.size() - 1)) {
+                    mActivityDetailBinding.alsoKnownTv.append("\n");
+                }
+            }
+        }else{
+            mActivityDetailBinding.alsoKnownTv.append(getString(R.string.placeHolder));
         }
 
         // {"name":{"mainName":"Ham and cheese sandwich","alsoKnownAs":[]},"placeOfOrigin":"","description":"A ham and cheese sandwich is a common type of sandwich. It is made by putting cheese and sliced ham between two slices of bread. The bread is sometimes buttered and/or toasted. Vegetables like lettuce, tomato, onion or pickle slices can also be included. Various kinds of mustard and mayonnaise are also common.","image":"https://upload.wikimedia.org/wikipedia/commons/thumb/5/50/Grilled_ham_and_cheese_014.JPG/800px-Grilled_ham_and_cheese_014.JPG","ingredients":["Sliced bread","Cheese","Ham"]}
